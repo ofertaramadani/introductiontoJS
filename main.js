63943443502e0e6f7e97634471ffc0646fda792f
ofertaramadani <oferte.ramadani@student.uni-pr.edu>
2022-11-10 19:58:00 +0100

    frst cmt

---
+++ main.js
@@ -0,0 +1,53 @@
+
+// Request a user’s name and display the response “Hello, [name]”.
+// Request a user’s year of birth, count his age, and display the result. Store the current year as a constant.
+// Request a length of a side of a square from a user and display the perimeter of the square.
+// Request a radius of a circle and display the area of such a circle.
+// Request a distance in km between the two cities from a user, and the time they want to cover it within. Count the speed needed to be on time.
+// Create a currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a constant.
+// A user states a flash drive memory in GB. The program has to count how many 820MB files can be stored on this flash drive.
+// A user types in an amount of money in a wallet and the price of one chocolate bar. The program counts how many chocolate bars the user can buy, and how much change will he/she have left.
+// The user types in a sum of bank deposits for 2 months with a yearly interest rate of 5%. The program counts the sum of interest.
+
+//d1
+// let name=prompt("Name:");
+// console.log('Hello'+' '+ name);
+
+
+//d2
+// let birthyear=prompt("Year of birth:")*1;
+// let date =  new Date().getFullYear();
+// console.log("You are: "+(date-birthyear)+" years old");
+
+//d3 
+// let side=prompt("Side of the square")*1;
+// console.log("Perimetri eshte:",side);
+
+//d4
+// let radius=prompt("Radius");
+// console.log("Area:",(Math.PI*radius*radius));
+
+//d5
+// let distance=prompt("Distance in km:");
+// let time=prompt("Time in hours");
+// console.log("Speed:",(distance/time));
+
+//d6
+// let usd=prompt("USD:");
+// console.log("USD TO EURO:",(usd/1.2));
+
+//d7
+// let gb=prompt("GBS:");
+// console.log("You can store:",((gb*1000)/820)," files");
+
+//d8
+// let money=prompt("Money u got:")*1;
+// let choco=prompt("Price of chocos u wants")*1;
+// let nums=money/choco;
+// console.log("You can buy:",nums," chocos and you have left:", money-(nums*choco),"euros");
+
+//d9
+// let sum=prompt("Sum of bank per 2months")*1;
+// let interest=(sum/2)*0.5;
+// console.log("Interest:",interest);
+
